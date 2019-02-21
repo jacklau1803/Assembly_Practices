97318785206246162f33514823cb5da84a112291
Jack Liu <liudun199903@gmail.com>
2019-02-20 23:57:58 -0800

    Lab4 added

---
+++ Lab4/Lab4.asm
@@ -0,0 +1,19 @@
+##########################################################################
+# Created by: Liu, Jack
+# dliu34
+# 20 Febuary 2019
+#
+# Assignment: Lab 4: ASCII Conversion
+# CMPE 012, Computer Systems and Assembly Language
+# UC Santa Cruz, Winter 2019
+#
+# Description: Read a string input and convert ASCII characters into base 4 number and print it out.
+#
+# Notes: This program is intended to be run from the MARS IDE.
+##########################################################################
+#psuedocode:
+#take inputs
+#convert by prefix
+#store
+#addition
+#print in base-4
