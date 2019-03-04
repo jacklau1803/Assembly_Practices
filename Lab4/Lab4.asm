7f67fbe8cf65412af0ffa571d08eeffe850ba747
Jack Liu <liudun199903@gmail.com>
2019-03-04 00:54:18 -0800

    lab4 added

--- Lab4/Lab4.asm
+++ Lab4/Lab4.asm
@@ -17,3 +17,156 @@
 #store
 #addition
 #print in base-4
+
+.data
+	msg1: .asciiz "You entered the numbers: \n"
+	msg2: .asciiz "The sum in base 4 is: \n"
+	ascii_array1: .space 10
+	ascii_array2: .space 10
+.text
+	#load args
+	lw 	$t0, 0($a1)
+	lw 	$t1, 4($a1)
+	
+	#print first msg
+	li 	$v0, 4
+	la 	$a0, msg1
+	syscall
+	
+	#print numbers
+	li 	$v0, 4
+	move 	$a0, $t0
+	syscall
+	
+	li 	$v0, 11
+	la 	$a0, 32
+	syscall
+	
+	li 	$v0, 4
+	move 	$a0, $t1
+	syscall
+	
+	li 	$v0, 11
+	la 	$a0, 10
+	syscall
+	
+	#print second msg
+	li 	$v0, 4
+	la 	$a0, msg2
+	syscall
+	
+	#hex or bin
+	lb 	$t2, 1($t0)
+	bge 	$t2, 120, hex1
+	j bin1
+	
+	hex1:
+		lb	$t2, 2($t0)
+		bge	$t2, 65, alph1
+		j num1
+		alph1:
+			subi $t3, $t2, 55
+			j con_hex1
+		num1:
+			subi $t3,$t2, 48
+			j con_hex1
+		con_hex1:
+			lb	$t2, 3($t0)
+			bge	$t2, 65, alph11
+			j num11
+			alph11:
+				subi $t4, $t2, 55
+				j con_hex11
+			num11:
+				subi $t4,$t2, 48
+				j con_hex11
+		con_hex11:
+			mul $t3, $t3, 16
+			add $s1, $t3, $t4
+			
+			j second
+	bin1:
+		move	$t3, $t0
+		addi	$t2, $zero, 0
+		addi	$t5, $zero, 0
+		j loop1
+		loop1:
+			lb 	$t4, 2($t3)
+			bge 	$t4, 49, addone1
+			j afteradd1
+			addone1:
+				addi 	$t5, $t5, 1
+				j afteradd1
+			afteradd1:
+				mul 	$t5, $t5, 2
+				j loop1_con
+			loop1_con:
+				la	$t3, 1($t3)
+				addi 	$t2, $t2, 1
+				ble	$t2, 7, loop1
+				move	$s1, $t5
+				j second	
+	second:
+		lb 	$t2, 1($t1)
+		bge 	$t2, 120, hex2
+		j bin2
+		hex2:
+		lb	$t2, 2($t1)
+		bge	$t2, 65, alph2
+		j num2
+		alph2:
+			subi $t3, $t2, 55
+			j con_hex2
+		num2:
+			subi $t3,$t2, 48
+			j con_hex2
+		con_hex2:
+			lb	$t2, 3($t1)
+			bge	$t2, 65, alph22
+			j num22
+			alph22:
+				subi $t4, $t2, 55
+				j con_hex22
+			num22:
+				subi $t4,$t2, 48
+				j con_hex22
+		con_hex22:
+			mul $t3, $t3, 16
+			add $s2, $t3, $t4
+			j sum
+	bin2:
+		move	$t3, $t1
+		addi	$t2, $zero, 0
+		addi	$t5, $zero, 0
+		loop2:
+			lb 	$t4, 2($t3)
+			bge 	$t4, 49, addone2
+			j afteradd2
+			addone2:
+				addi 	$t5, $t5, 1
+				j afteradd2
+			afteradd2:
+				mul 	$t5, $t5, 2
+				j loop2_con
+			loop2_con:
+				la	$t3, 1($t3)
+				addi 	$t2, $t2, 1
+				ble	$t2, 7, loop2
+				move	$s2, $t5
+				j sum
+	sum:
+		add 	$t6, $s1, $s2
+		li 	$v0, 1
+		andi 	$t6, $t6, 0xFF
+		
+		
+		
+		
+		move 	$a0, $s0
+		syscall
+		li 	$v0, 11
+		la 	$a0, 10
+		syscall
+		li	$v0, 10
+		syscall
+			
---
+++ Lab4/README.txt
@@ -0,0 +1,19 @@
+------------------------
+Lab 4: ASCII (HEX or 2SC) to Base 4
+CMPE 012 Winter 2019
+Liu, Jack
+CruzID
+-------------------------
+What was your approach to converting each ASCII input to two’s complement
+form?
+Convert each bit and add up.
+What did you learn in this lab?
+MIPS is so hard.
+Did you encounter any issues? Were there parts of this lab you found
+enjoyable?
+I encountered a lot of issues. I could have more time to deal with them, but my car got hit from behind on my way back to Santa Cruz and that took me serval hours to deal with.
+How would you redesign this lab to make it better?
+Ask for more help in the lab.
+Did you collaborate with anyone on this lab? Please list who you collaborated
+with and the nature of your collaboration.
+No.
